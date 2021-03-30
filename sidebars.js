252d92fb29e7ac47a821da1d215df227c1d82799
Alexey <akapitoha@replika.ai>
2021-03-30 09:56:48 +0300

    first commit

---
+++ sidebars.js
@@ -0,0 +1,16 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus Tutorial1',
+      items: [
+        'getting-started',
+        'create-a-page',
+        'create-a-document',
+        'create-a-blog-post',
+        'markdown-features',
+        'thank-you',
+      ],
+    },
+  ],
+};
