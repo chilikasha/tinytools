24d5f959e098e88f17f9615babd86ccb9fa74225
Alexey <tropinqa@gmail.com>
2021-04-19 00:17:56 +0300

    apps on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,45 +14,38 @@ const h1Style = {
   // margin: '4px',
 }
 
-const pStyle = {
-  margin: '4px',
-}
-
 const buttonStyle = {
   'text-align': 'left',
 }
 
 const features = [
-  // {
-  //   title: 'Easy to Use',
-  //   imageUrl: 'img/undraw_docusaurus_mountain.svg',
-  //   description: (
-  //     <>
-  //       Docusaurus was designed from the ground up to be easily installed and
-  //       used to get your website up and running quickly.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Focus on What Matters',
-  //   imageUrl: 'img/undraw_docusaurus_tree.svg',
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   imageUrl: 'img/undraw_docusaurus_react.svg',
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
+  {
+    // title: 'qView',
+    imageUrl: 'qview.png',
+    description: (
+      <>
+        Fast <a href="/docs/images/qview">image viewer</a>.
+      </>
+    ),
+  },
+  {
+    // title: 'Marta',
+    imageUrl: 'marta.png',
+    description: (
+      <>
+        Native dual-pane macOS <a href="/docs/files/marta">file manager</a>.
+      </>
+    ),
+  },
+  {
+    // title: 'Tiny Player',
+    imageUrl: 'tinyplayer.png',
+    description: (
+      <>
+        Music <a href="/docs/music/tiny-player">player</a> for Apple ecosystem.
+      </>
+    ),
+  },
 ]
 
 function Feature({ imageUrl, title, description }) {
@@ -76,7 +69,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Curated list of fast, minimalistic, beautiful apps and tools,
+      description="Curated list of fast, minimalist, beautiful apps and tools,
       which solve real problems. No fluff."
     >
       <head>
@@ -88,7 +81,7 @@ export default function Home() {
       <header className={clsx('hero--primary', styles.heroBanner)}>
         {/* <header className={clsx('hero hero--primary', styles.heroBanner)}> */}
         {/* <img src="img/minimize_light.svg" alt=""></img>*/}
-        <svg
+        {/*<svg
           xmlns="http://www.w3.org/2000/svg"
           width="44"
           height="44"
@@ -102,15 +95,11 @@ export default function Home() {
         >
           <path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"></path>
           <style> @media (prefers-color-scheme: dark)</style>
-        </svg>
+        </svg>*/}
         <div className="container">
-          <h1 style={h1Style} className="hero__title">
-            {siteConfig.title}
-          </h1>
-          <p style={pStyle} className="hero__subtitle">
-            {siteConfig.tagline}
-          </p>
-          <p>Fast, beautiful, minimalistic.</p>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <p>Fast • beautiful • minimalist • single-purpose apps.</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
@@ -123,7 +112,7 @@ export default function Home() {
             </Link>
           </div>
           <br></br>
-          <p>Updates subscription coming soon...</p>
+          {/*<p>Updates subscription coming soon...</p>*/}
         </div>
       </header>
       <main>
