781e49a79689bf1b3f277d84d9df6168831b3028
Alex <tropinqa@gmail.com>
2022-02-19 20:47:18 +0100

    meta, keywords

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,9 +46,15 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
         metadata: [
-        {name: 'keywords', content: 'useful, apps, simple, tiny, single-purpose, tools, list, collection, macos, web apps'},
-        {name: 'og:type', content: 'website'},
-        {name: 'og:url', content: 'https://tinytools.club/'},
+        {name: 'keywords', content: 'small useful apps, useful apps for macos, interesting apps, list of useful apps, minimalist software, simple macos tools, list of useful apps for macos, most useful apps, tiny macos apps, small but useful apps'},
+        {property: 'og:type', content: 'website'},
+        {name: 'description', content: 'Collection of useful, simple, single-purpose apps and tools for everyday problems and tasks'},
+        {property: 'og:title', content: 'Tiny tools'},
+        {property: 'og:image', content: '/img/tt.webp'},
+        {property: 'og:description', content: 'Collection of useful, simple, single-purpose apps and tools for everyday problems and tasks'},
+        {property: 'og:url', content: 'https://tinytools.club/'},
+        {name: 'twitter:card', content: 'summary_large_image'},
+        {name: 'twitter:image', content: '/img/tt.webp'},
         {name: 'twitter:description', content: 'Collection of useful, simple, single-purpose apps and tools for everyday problems and tasks'},
         {name: 'ahrefs-site-verification', content: 'e84720fd3dd7d0e2d439a09168118c870f2382eb512d13636e43d3af505252f5'}
       ], 
