aa7e809b3c8be5d5ae64510b0f40147711831490
Alex <tropinqa@gmail.com>
2022-04-19 20:44:52 +0200

    panelbear

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import 'nextra-theme-docs/style.css'
 import '../public/style.css'
 import Script from "next/script"
+import { usePanelbear } from '@panelbear/panelbear-nextjs'
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <>
-      <script data-no-cookie async src="https://cdn.splitbee.io/sb.js" />
-      <Component {...pageProps} />
-    </>
-  )
+  usePanelbear('JHnEG7vc0FG', {
+    debug: false
+  })
+  return (<Component {...pageProps} />)
 }
