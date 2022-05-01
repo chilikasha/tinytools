dd7b216817c968d740484f1aaaf9129e768b2d2b
Alex <tropinqa@gmail.com>
2022-05-01 15:32:01 +0200

    updates

--- theme.config.js
+++ theme.config.js
@@ -22,17 +22,17 @@ export default {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#ffffff" />
-      <meta name="description" content="Tiny tools: collection of useful, simple, single-purpose apps and tools for everyday problems and tasks" />
+      <meta name="description" content="Tiny tools: list of useful, simple, single-purpose apps and tools for everyday problems and tasks" />
       <meta name="og:title" content="Tiny tools" />
       <meta name="og:type" content="website" />
       <meta name="og:image" content="https://tinytools.club/tt.webp" />
-      <meta name="og:description" content="Collection of useful, simple, single-purpose apps and tools for everyday problems and tasks" />
+      <meta name="og:description" content="List of useful, simple, single-purpose apps and tools for everyday problems and tasks" />
       <meta name="og:url" content="https://tinytools.club/" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://tinytools.club/tt.webp" />
       <meta name="twitter:url" content="https://tinytools.club" />
       <meta name="twitter:site:domain" content="https://tinytools.club" />
-      <meta name="twitter:description" content="Tiny tools: collection of useful, simple, single-purpose apps and tools for everyday problems and tasks" />
+      <meta name="twitter:description" content="Tiny tools: list of useful, simple, single-purpose apps and tools for everyday problems and tasks" />
       <meta name="ahrefs-site-verification" content="e84720fd3dd7d0e2d439a09168118c870f2382eb512d13636e43d3af505252f5" />
       <link
         rel="icon"
