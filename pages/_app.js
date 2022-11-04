3ad6f370f59dad1fd11cff57e7a9b23d4c79027e
Alex <tropinqa@gmail.com>
2022-11-04 13:20:54 +0300

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import 'nextra-theme-docs/style.css'
 import '../public/style.css'
 import { Analytics } from '@vercel/analytics/react'
-// import { usePanelbear } from '@panelbear/panelbear-nextjs'
 
-export default function Nextra({ Component, pageProps }) {
-  // usePanelbear('JHnEG7vc0FG', {
-  //   debug: false
-  // })
-  const getLayout = Component.getLayout || ((page) => page)
-  return getLayout(<Component {...pageProps} />)
-  return <Analytics />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
